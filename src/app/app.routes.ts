7f7ef355f2a9ae3d0b5b2f085e85ca11c52edd76
Błażej <blazejbart@gmail.com>
2026-02-06 16:26:58 +0100

    feat: add routes for creating and editing tasks

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,23 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'tasks',
-    loadComponent: () =>
-      import('./features/tasks/pages/task-list/task-list').then((m) => m.TaskList),
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./features/tasks/pages/task-list/task-list').then((m) => m.TaskList),
+      },
+      {
+        path: 'new',
+        loadComponent: () =>
+          import('./features/tasks/pages/task-form/task-form').then((m) => m.TaskForm),
+      },
+      {
+        path: ':id/edit',
+        loadComponent: () =>
+          import('./features/tasks/pages/task-form/task-form').then((m) => m.TaskForm),
+      },
+    ],
   },
-  {
-    path: '',
-    redirectTo: 'tasks',
-    pathMatch: 'full',
-  },
+  { path: '', redirectTo: 'tasks', pathMatch: 'full' },
 ];
