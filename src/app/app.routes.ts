f29a591729f207e14e6adcf6afce606161a5dbae
Błażej <blazejbart@gmail.com>
2026-02-06 13:58:13 +0100

    feat: add tasks route and task list page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'tasks',
+    loadComponent: () =>
+      import('./features/tasks/pages/task-list/task-list').then((m) => m.TaskList),
+  },
+  {
+    path: '',
+    redirectTo: 'tasks',
+    pathMatch: 'full',
+  },
+];
