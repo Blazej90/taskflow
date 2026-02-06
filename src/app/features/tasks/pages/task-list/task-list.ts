7d4f7942932e27c10cc3e425cc2b562ce956cf1c
Błażej <blazejbart@gmail.com>
2026-02-06 17:45:22 +0100

    feat: add task filtering and new task button

--- src/app/features/tasks/pages/task-list/task-list.ts
+++ src/app/features/tasks/pages/task-list/task-list.ts
@@ -1,25 +1,42 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { TaskCard } from '../../components/task-card/task-card';
-import { Task } from '../../task';
+import { Task, TaskStatus } from '../../task';
 import { TasksService } from '../../tasks.service';
+
+type Filter = 'all' | TaskStatus;
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [CommonModule, TaskCard],
+  imports: [CommonModule, RouterLink, TaskCard],
   templateUrl: './task-list.html',
   styleUrl: './task-list.scss',
 })
 export class TaskList {
-  tasks: Task[];
+  tasks: Task[] = [];
+  filter: Filter = 'all';
 
   constructor(private tasksService: TasksService) {
-    this.tasks = this.tasksService.getAll();
+    this.refresh();
+  }
+
+  get filteredTasks(): Task[] {
+    if (this.filter === 'all') return this.tasks;
+    return this.tasks.filter((t) => t.status === this.filter);
+  }
+
+  setFilter(filter: Filter) {
+    this.filter = filter;
   }
 
   removeTask(id: string) {
     this.tasksService.delete(id);
+    this.refresh();
+  }
+
+  private refresh() {
     this.tasks = this.tasksService.getAll();
   }
 }
