076ca54d344657f38be50f0df7df49eea9aee3c1
Błażej <blazejbart@gmail.com>
2026-02-08 14:31:16 +0100

    feat: disable task card actions durinf per-task loading

--- src/app/features/tasks/components/task-card/task-card.ts
+++ src/app/features/tasks/components/task-card/task-card.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+
 import { Task } from '../../task';
+import { TasksService } from '../../tasks.service';
 
 @Component({
   selector: 'app-task-card',
@@ -11,8 +13,22 @@ import { Task } from '../../task';
   styleUrl: './task-card.scss',
 })
 export class TaskCard {
+  private tasksService = inject(TasksService);
+
   @Input({ required: true }) task!: Task;
 
   @Output() remove = new EventEmitter<string>();
   @Output() toggle = new EventEmitter<string>();
+
+  get isDeleting(): boolean {
+    return this.tasksService.isDeleting(this.task.id);
+  }
+
+  get isUpdating(): boolean {
+    return this.tasksService.isUpdating(this.task.id);
+  }
+
+  get isBusy(): boolean {
+    return this.isDeleting || this.isUpdating;
+  }
 }
