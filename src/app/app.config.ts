08e471887fdee3b81c152e1722cc72c367d08023
Błażej <blazejbart@gmail.com>
2026-02-22 14:16:32 +0100

    refactor(config): move Firebase to runtime config and setup Vercel env endpoint

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,21 +1,3 @@
-// import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-// import { provideRouter } from '@angular/router';
-// import { provideAnimations } from '@angular/platform-browser/animations';
-// import { routes } from './app.routes';
-
-// import { TASKS_REPOSITORY } from './features/tasks/task.repository';
-// import { FirestoreTasksRepository } from './features/tasks/tasks.repository.firestore';
-
-// export const appConfig: ApplicationConfig = {
-//   providers: [
-//     provideBrowserGlobalErrorListeners(),
-//     provideRouter(routes),
-//     provideAnimations(),
-
-//     { provide: TASKS_REPOSITORY, useClass: FirestoreTasksRepository },
-//   ],
-// };
-
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
--- src/app/firebase.config.ts
+++ src/app/firebase.config.ts
@@ -1,9 +1,3 @@
-// import { initializeApp } from 'firebase/app';
-// import type { FirebaseApp } from 'firebase/app';
-// import { environment } from '@/../environments/environment';
-
-// export const firebaseApp: FirebaseApp = initializeApp(environment.firebase);
-
 import { initializeApp } from 'firebase/app';
 import type { FirebaseApp } from 'firebase/app';
 
